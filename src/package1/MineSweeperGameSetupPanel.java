382af117ce3bae774d6d9a3dc059e259a01ea0df
4fab <kvonehr@gmail.com>
2014-02-19 22:16:11 -0500

    Comments

--- src/package1/MineSweeperGameSetupPanel.java
+++ src/package1/MineSweeperGameSetupPanel.java
@@ -18,13 +18,15 @@ public class MineSweeperGameSetupPanel implements ActionListener{
 	private JLabel gameLabel = new JLabel("Game Setup");
 	private String[] size1 = new String[30];
 	private String[] mine1 = new String[10];
-    private JFrame frame = new JFrame("MineSweeper");
-    private JComboBox mines;
-    private JComboBox size;
+	private JFrame frame = new JFrame("MineSweeper");
+    	private JComboBox mines;
+    	private JComboBox size;
     	
 	public int wins = 0;
 	public int loses = 0;
-		
+	
+	
+	//creates a frame for the setup of the game	
 	MineSweeperGameSetupPanel(){
 	
 		
@@ -41,9 +43,11 @@ public class MineSweeperGameSetupPanel implements ActionListener{
 		gamePanel.setBorder(new EmptyBorder(20, 10, 0, 0) );
 		optionPanel.setBorder(new EmptyBorder(0, 20, 20, 20) );
 		
+		//populates the array for the mine combo box options
 		for(int i=0; i<10;i++){
 			mine1[i] = Integer.toString(i+1);
 		}
+		//populates the arry for the size combo box options
 		for(int i=0; i<30;i++){
 			size1[i] = Integer.toString(i+1);
 		}
@@ -80,6 +84,12 @@ public class MineSweeperGameSetupPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		//when the start button is pressed, begin a new game,
+		//set the loses and wins and refresh the board to reflect
+		//the loses and wins. Default is 0, 0. When the user requests for
+		//a new board from the minesweeperpanel, the wins and loses in this 
+		//class are set to the current score and then sent back to the new 
+		//board created by the new minesweeper panel. 
 		if(e.getSource() == startButton){
 			int mines = this.mines.getSelectedIndex() + 1;
 			int size = this.size.getSelectedIndex() + 1;
@@ -89,28 +99,29 @@ public class MineSweeperGameSetupPanel implements ActionListener{
 			gamePanel.displayBoard();
 			frame.dispose();
 		}
+		//exits the frame and game
 		if(e.getSource() == quitButton){
 			System.exit(0);
 		}
 		
 	}
 
-
+	//gets the number of wins, default 0
 	public int getWins() {
 		return wins;
 	}
 
-
+	//sets the nubmer of wins
 	public void setWins(int wins) {
 		this.wins = wins;
 	}
 
-
+	//gets the number of loses, default 0
 	public int getLoses() {
 		return loses;
 	}
 
-
+	//sets the number of loses
 	public void setLoses(int loses) {
 		this.loses = loses;
 	}
