cf99db25f819af48a26c29efea6e9814ab75e73a
4fab <kvonehr@gmail.com>
2014-02-19 22:00:45 -0500

    Comments

--- src/package1/MineSweeper.java
+++ src/package1/MineSweeper.java
@@ -6,6 +6,7 @@ public class MineSweeper {
 	
 	public static void main(String[] args) {
 
+		//Loads Setup Panel for a new MineSweeper Game
 		MineSweeperGameSetupPanel game = new MineSweeperGameSetupPanel();
 		
 	}
